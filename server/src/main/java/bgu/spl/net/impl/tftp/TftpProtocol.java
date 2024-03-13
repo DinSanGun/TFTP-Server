98702356ce9b9687e044ea1e8442fa2ba2ea0fb0
Din Yair <dinsadot@gmail.com>
2024-03-14 01:22:23 +0200

    Add ClientDownloadRequest method, add helper method createDataPacket

--- server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
+++ server/src/main/java/bgu/spl/net/impl/tftp/TftpProtocol.java
@@ -1,7 +1,11 @@
 package bgu.spl.net.impl.tftp;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FilenameFilter;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -20,7 +24,7 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
     private int connectionId;
     private Connections<byte[]> connections;
 
-    private final byte[] genericAckPacket = {0 , 0 , 4 , 0};
+    private final byte[] genericAckPacket = {0 , 4 , 0 , 0};
 
     @Override
     public void start(int connectionId, Connections<byte[]> connections) {
@@ -36,18 +40,18 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
                                 // first of them is zero according to the instructions
 
         switch (op_code) {
-            case 1: break;
-            case 2: break;
+            case 1: clientDownloadRequest(message); break;
+            case 2: clientUploadRequest(message); break;
             case 3: break;
             case 4: break;
             case 5: break;
-            case 6: directoryList(); break;
+            case 6: directoryList(); break; //STILL NEED TO ADD ACKNOWLDEGMENT BETWEEN PACKETS
             case 7: loginUser(message); break;
             case 8: deleteFile(message); break;
             case 9: break;
             case 10: disconnectUser(); break;
             default: connections.send(connectionId, errorPacket(4)); break;
-        }
+        } 
     }
 
 
@@ -67,12 +71,60 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
         String username = new String(usernameInBytes, StandardCharsets.UTF_8);
 
         if( connections.login(connectionId , username) ) { //Login succeeded
-            byte[] ackPacket = {0 , 4 , 0 , 0};
-            connections.send(connectionId, ackPacket);
+            connections.send(connectionId, genericAckPacket);
         }
         else { //Create and send error packet
             byte[] errorPacket = errorPacket(7); //User already logged in
             connections.send(connectionId, errorPacket);
+        }
+    }
+
+    private void clientUploadRequest(byte[] message){
+        
+    }
+
+    private void clientDownloadRequest(byte[] message) {
+
+        byte[] filenameInBytes = Arrays.copyOfRange(message, 2, message.length - 1);
+        String filename = new String(filenameInBytes , StandardCharsets.UTF_8);
+        FileInputStream fileAsStream;        
+
+        try{
+            fileAsStream = new FileInputStream("server\\Files" + filename);
+            
+            int dataPacketSize = 512 + 6;
+            int blockNumber = 1;
+            //Creating new DATA packet
+            byte[] dataPacket = createDataPacket(blockNumber , dataPacketSize);
+
+            byte nextByte;
+            int index = 6;
+            while( (nextByte = (byte) fileAsStream.read()) >= 0) {
+
+                dataPacket[index] = nextByte;
+                index++;
+
+                if(index == dataPacket.length - 1) {
+                    dataPacket[index] = 0;
+                    connections.send(connectionId, dataPacket);
+                    blockNumber++;
+                    dataPacket = createDataPacket(blockNumber, dataPacketSize);
+                    index = 0;
+                }
+            }
+
+            if(index != 0) {
+                int lastDataPacketSize = index + 2; //+2 because 1 extra byte for termination byte zero, and another one because index starts with zero, hence size is + 2.
+                byte[] lastDataPacket = createDataPacket(blockNumber, lastDataPacketSize); 
+                System.arraycopy(dataPacket, 0, lastDataPacket, 0, index + 1);
+                lastDataPacket[lastDataPacketSize - 1] = 0;
+                connections.send(connectionId, lastDataPacket);
+            }
+
+        } catch(FileNotFoundException e) {
+            connections.send(connectionId, errorPacket(1));
+        } catch(IOException ex) {
+            ex.printStackTrace();
         }
     }
     
@@ -85,7 +137,7 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
         if(!fileToDelete.exists())
             connections.send(connectionId, errorPacket(1));
         else {
-
+            connections.send(connectionId, genericAckPacket);
             fileToDelete.delete();
             byte deleted = 0;
             broadcast(filename, deleted);
@@ -206,11 +258,23 @@ public class TftpProtocol implements BidiMessagingProtocol<byte[]>  {
      * @param dir - the directory to lookup
      * @return a set of all the file names in the specified directory
      */
-    public List<String> listFilesInDirectory(String dir) {
+    private List<String> listFilesInDirectory(String dir) {
     return Stream.of(new File(dir).listFiles())
       .filter(file -> !file.isDirectory())
       .map(File::getName)
       .collect(Collectors.toList());
     }
     
+
+    private byte[] createDataPacket(int blockNumber, int packetSize) {
+
+        byte[] dataPacket = new byte[packetSize];
+        dataPacket[0] = 0;
+        dataPacket[1] = 3; //DATA OP_CODE
+        dataPacket[2] = (byte) (packetSize >> 8); //Packet size - 2 bytes
+        dataPacket[3] = (byte) (packetSize & 0xff);
+        dataPacket[4] = (byte) (blockNumber >> 8); //Block number - 2 bytes
+        dataPacket[5] = (byte) (blockNumber & 0xff);
+        return dataPacket;
+    }
 }
