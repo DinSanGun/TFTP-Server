74ce0e985d635b7822f26bbc94d833e77ddf9ad3
Din Yair <dinsadot@gmail.com>
2024-03-18 22:35:11 +0200

    Add implementation of client side's messaging protocol and encoder decoder - implemented by assignment's partner Estee Cohen

--- client/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpEncoderDecoder.java
@@ -1,5 +1,87 @@
 package bgu.spl.net.impl.tftp;
 
-public class TftpEncoderDecoder {
-    
+import bgu.spl.net.api.MessageEncoderDecoder;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TftpEncoderDecoder implements MessageEncoderDecoder<byte[]> {
+    //list to store bytes of the incoming message
+    private List<Byte> bytesList;
+    //OpCode for message identification
+    private short opCode;
+
+    public TftpEncoderDecoder() {
+        this.bytesList = new LinkedList<>();
+    }
+
+    @Override
+    public byte[] decodeNextByte(byte nextByte) {
+        //if the bytes list is empty, add the next byte
+        if (bytesList.isEmpty()) {
+            bytesList.add(nextByte);
+            return null; //Continue collecting bytes
+        } else
+            bytesList.add(nextByte);
+
+        if (bytesList.size() == 2) {
+            //extract OpCode from the first two bytes
+            byte[] opCodeBytes = new byte[]{bytesList.get(0), bytesList.get(1)};
+            this.opCode = (short) (((short) opCodeBytes[0] & 0xFF) << 8 | (short) (opCodeBytes[1] & 0xFF));
+        }
+
+        //decode message based on OpCode
+        switch (opCode) {
+            case (3): //data message
+                if (bytesList.size() > 3) {
+                    byte[] sizeBytes = new byte[]{bytesList.get(2), bytesList.get(3)};
+                    int packetSize = (short) (((short) sizeBytes[0] & 0xFF) << 8 | (short) (sizeBytes[1] & 0xFF));
+                    if (bytesList.size() == packetSize + 6)
+                        return bytesToArray(); //complete message received
+                    else
+                        return null; //incomplete message, continue collecting bytes
+                }
+            case (4): //acknowledgement message
+            case (10): //disconnect message
+            case (6): //directory inquiry message
+                if (bytesList.size() == 2)
+                    return bytesToArray(); //complete message received
+                else
+                    return null; //incomplete message, continue collecting bytes
+            case (9): //broadcast message
+            case (5): //error message
+                if (bytesList.size() > 3) {
+                    if (nextByte == 0)
+                        return bytesToArray(); //complete message received
+                    else
+                        return null; //incomplete message, continue collecting bytes
+                }
+        }
+
+        // If the next byte is zero, it indicates the end of a message
+        if (nextByte == 0)
+            return bytesToArray(); // Complete message received
+
+        return null; //incomplete message, continue collecting bytes
+    }
+
+    //---------------------- ENCODER ----------------------//
+    @Override
+    public byte[] encode(byte[] message) {
+        return message; 
+    }
+
+    //------------------ HELPER FUNCTION ------------------//
+    // Convert bytes list to byte array and clear list for next message
+    private byte[] bytesToArray() {
+        byte[] result = new byte[bytesList.size()];
+        int i = 0;
+        for (Byte b : bytesList) {
+            result[i] = b;
+            i++;
+        }
+
+        bytesList.clear(); //clear the bytes list for the next message
+        this.opCode = -1; //reset OpCode
+        return result;
+    }
 }
--- client/src/main/java/bgu/spl/net/impl/tftp/TftpMassagingProtocol.java
+++ client/src/main/java/bgu/spl/net/impl/tftp/TftpMassagingProtocol.java
@@ -1,5 +1,286 @@
 package bgu.spl.net.impl.tftp;
 
-public class TftpMassagingProtocol {
-    
+import bgu.spl.net.api.MessagingProtocol;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class TftpMassagingProtocol implements MessagingProtocol<byte[]> {
+    private boolean terminate = false;
+    private short lastBlock = 0;
+    private Queue<byte[]> packets = new ConcurrentLinkedQueue<>();
+    private Opcode lastOpcode = Opcode.UNKNOWN;
+    private final String directoryPath = "";
+    private String fileTransferred = "";
+    private ArrayDeque<Byte> currentDir = new ArrayDeque<>();
+    String lastArg;
+
+    private enum Opcode { // Enum representing supported TFTP opcodes
+        RRQ(1), WRQ(2), DATA(3), ACK(4), ERROR(5), DIRQ(6), LOGRQ(7), DELRQ(8), BCAST(9), DISC(10), UNKNOWN(-1);
+        
+        private final short value;
+
+        Opcode(int value) { this.value = (short) value;}
+
+        public byte[] toBytes() { return new byte[]{(byte) (value >> 8), (byte) (value & 0xff)};}
+
+        public static Opcode fromInt(int value) {
+            for (Opcode opcode : Opcode.values()) {
+                if (opcode.value == value) {
+                    return opcode;
+                }
+            }
+            return UNKNOWN;
+        }
+
+        public static Opcode fromBytes(byte a, byte b) { return fromInt((short) (((short) a) << 8 | (short) (b) & 0x00ff));}
+
+        public static Opcode extract(byte[] msg) { return fromBytes(msg[0], msg[1]); }
+    }
+
+    // Error messages
+    private static final byte[] NOT_DEFINED = "Not defined".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] FILE_NOT_FOUND = "File not found".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] ILLEGAL_OP = "Illegal operation".getBytes(StandardCharsets.UTF_8);
+
+
+    @Override
+    public byte[] process(byte[] message) {
+        Opcode opcode = Opcode.extract(message);
+        byte[] response = null;
+
+        switch (opcode) {
+            case DATA:
+                response = handleData(message);
+                break;
+            case ACK:
+                response = handleAck(message);
+                break;
+            case ERROR:
+                handleError(message);
+                break;
+            case BCAST:
+                handleBroadcast(message);
+                break;
+            case UNKNOWN:
+                response = createErrorMessage(NOT_DEFINED);
+                break;
+            case RRQ:
+            case WRQ:
+                lastArg = new String(message, 2, message.length - 3, StandardCharsets.UTF_8);
+            default:
+                lastOpcode = opcode;
+        }
+
+        return response;
+    }
+
+    @Override
+    public boolean shouldTerminate() { return terminate; }
+
+    private byte[] createErrorMessage(byte[] err) {
+        byte[] pac = new byte[5 + err.length];
+
+        pac[0] = Opcode.ERROR.toBytes()[0];
+        pac[1] = Opcode.ERROR.toBytes()[1];
+        pac[2] = NOT_DEFINED[0];
+        pac[3] = NOT_DEFINED[1];
+
+        System.arraycopy(err, 0, pac, 4, err.length);
+        pac[pac.length - 1] = 0;
+
+        return pac;
+    }
+
+    private byte[] handleAck(byte[] message) {
+        short ackBlock = (short) ((message[2] << 8) | (message[3] & 0xFF));
+        Opcode cmdOpcode;
+        byte[] lastPacket = null;
+
+        if (ackBlock == 0)
+            cmdOpcode = lastOpcode;
+        else if (packets.isEmpty())
+            return createErrorMessage(NOT_DEFINED);
+        else {
+            lastPacket = packets.peek();
+            cmdOpcode = Opcode.extract(lastPacket);
+        }
+
+        //extract block number from DATA packet if exists
+        short packetBlock = cmdOpcode == Opcode.DATA
+                ? (short) ((lastPacket[4] << 8) | (lastPacket[5] & 0xFF))
+                : 0;
+        byte[] response = null;
+
+        if (packetBlock != ackBlock)
+            return createErrorMessage(NOT_DEFINED);
+
+        String filename;
+
+        switch (cmdOpcode) {
+            case WRQ:
+                filename = lastArg;
+                addPackets(filename);
+                fileTransferred = filename;
+                response = packets.peek();
+                break;
+            case DATA:
+                packets.remove();
+                response = packets.peek();
+
+                if (response == null) {
+                    System.out.println("WRQ " + fileTransferred + " complete");
+                    fileTransferred = "";
+                }
+
+                break;
+            case LOGRQ:
+            case DELRQ:
+                break;
+            case DISC:
+                terminate = true;
+                break;
+            default:
+                response = createErrorMessage(ILLEGAL_OP);
+                break;
+        }
+
+        return response;
+    }
+
+    // Add packets from file to the queue
+    private void addPackets(String filename) {
+        File fileToSend = new File(directoryPath + File.separator + filename);
+
+        if (!fileToSend.exists())
+            System.out.println(new String(FILE_NOT_FOUND, StandardCharsets.UTF_8));
+
+        try (FileInputStream fstream = new FileInputStream(fileToSend)) {
+            ArrayDeque<Byte> packetData = new ArrayDeque<>();
+            int nextByte;
+            byte[] packet;
+
+            lastBlock = 0;
+
+            while ((nextByte = fstream.read()) != -1) {
+                packetData.add((byte) nextByte);
+
+                // Assuming MAX_DATA_PACKET is 512
+                if (packetData.size() == 512) {
+                    packet = buildPacket(packetData, ++lastBlock);
+                    packets.add(packet);
+                    packetData.clear();
+                }
+            }
+
+            packet = buildPacket(packetData, ++lastBlock);
+            packets.add(packet);
+        } catch (IOException ignored) {
+            System.out.println(ignored.getMessage());
+        }
+    }
+
+    //handle incoming DATA packet
+    private byte[] handleData(byte[] packet) {
+        short block = (short) ((packet[4] << 8) | (packet[5] & 0xFF));
+        short packetSize = (short) ((packet[2] << 8) | (packet[3] & 0xFF));
+        byte[] bytes;
+        File file;
+
+        if (lastOpcode == Opcode.RRQ) {
+            file = new File(directoryPath + File.separator + lastArg);
+
+            try (FileOutputStream fStream = new FileOutputStream(file, true)) {
+                fStream.write(packet, 6, packet.length - 6);
+            } catch (IOException ignored) {
+            } finally {
+                // Assuming MAX_DATA_PACKET is 512
+                if (packetSize < 512) {
+                    System.out.println("RRQ " + file.getName() + " complete");
+                    file = null;
+                }
+            }
+        } else {
+            for (int i = 6; i < packet.length; i++) {
+                if (packet[i] == 0) {
+                    bytes = new byte[currentDir.size()];
+
+                    for (int j = 0; j < bytes.length; j++) {
+                        bytes[j] = (byte) currentDir.removeFirst();
+                    }
+
+                    System.out.println(new String(bytes, StandardCharsets.UTF_8));
+                    currentDir.clear();
+                } else
+                    currentDir.add(packet[i]);
+            }
+
+            //assuming MAX_DATA_PACKET is 512
+            if (packetSize < 512) {
+                bytes = new byte[currentDir.size()];
+
+                for (int j = 0; j < bytes.length; j++) {
+                    bytes[j] = (byte) currentDir.removeFirst();
+                }
+
+                System.out.println(new String(bytes, StandardCharsets.UTF_8));
+                currentDir.clear();
+            }
+        }
+
+        return buildAckPacket(block);
+    }
+
+    private void handleError(byte[] packet) {
+        short errNum = (short) ((packet[2] << 8) | (packet[3] & 0xFF));
+        String msg = new String(packet, 4, packet.length - 5, StandardCharsets.UTF_8);
+        System.out.println("Error " + errNum + " (" + msg + ")");
+    }
+
+    private void handleBroadcast(byte[] message) {
+        boolean added = message[2] == 1;
+        String filename = new String(message, 3, message.length - 4, StandardCharsets.UTF_8);
+        System.out.print("BCAST ");
+
+        if (added)
+            System.out.print("add ");
+        else
+            System.out.print("del ");
+
+        System.out.println(filename);
+    }
+
+    //build DATA packet
+    private byte[] buildPacket(ArrayDeque<Byte> bytes, short block) {
+        byte[] packet = new byte[6 + bytes.size()];
+        int bytesSize = bytes.size();
+
+        packet[0] = Opcode.DATA.toBytes()[0];
+        packet[1] = Opcode.DATA.toBytes()[1];
+        packet[2] = (byte) (bytesSize >> 8);
+        packet[3] = (byte) (bytesSize & 0xFF);
+        packet[4] = (byte) (block >> 8);
+        packet[5] = (byte) (block & 0xFF);
+
+        for (int j = 0; j < bytesSize; j++)
+            packet[6 + j] = bytes.removeFirst();
+
+        return packet;
+    }
+
+    //build ACK packet
+    private byte[] buildAckPacket(short block) {
+        byte[] packet = new byte[4];
+        packet[0] = Opcode.ACK.toBytes()[0];
+        packet[1] = Opcode.ACK.toBytes()[1];
+        packet[2] = (byte) (block >> 8);
+        packet[3] = (byte) (block & 0xFF);
+        return packet;
+    }
 }
